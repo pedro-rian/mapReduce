cadaeadad1207e9b35d576b23443cbb42c5a5944
Gabriel Rodrigues <gab_gro@hotmail.com>
2024-06-07 18:41:47 -0300

    prototipo contagem de palavras

--- map_reduce/lib/map_reduce.ex
+++ map_reduce/lib/map_reduce.ex
@@ -1,11 +1,16 @@
-defmodule Utils do
+defmodule Mapreduce do
+
+  def ler(arquivo \\ "dados.txt") do
+    arquivo |> File.read |> elem(1) |> String.replace(".","") |> String.replace(",","") |> String.split(" ")
+  end
+
   def partitioning(enumerable, tasks) do
     size = div(Enum.count(enumerable), tasks)
     partitioning(enumerable, tasks, size, 0)
   end
 
   defp partitioning(enumerable, tasks, size, cont) when cont < tasks do
-    [Task.async(Enum, :slice, [enumerable, cont * size, size])] ++
+    [Task.async(fn -> listToMap(enumerable)|> Enum.slice(cont * size, size ) |> agrupar  end)] ++
       partitioning(enumerable, tasks, size, cont + 1)
   end
 
@@ -13,26 +18,63 @@ defmodule Utils do
     []
   end
 
-  def runTasks([]), do: []
 
-  def runTasks(tasks) do
-    [Task.await(hd(tasks))] ++ runTasks(tl(tasks))
+  def runtask([]) do
+    []
   end
 
-  def reduce(results) do
-    Enum.reduce(results, %{}, fn chunk, acc ->
-      Enum.reduce(chunk, acc, fn elem, acc ->
-        Map.update(acc, elem, 1, &(&1 + 1))
-      end)
+  def runtask([h|t]) do
+    Task.await(h) ++ runtask(t)
+  end
+
+
+  def listToMap([]) do
+    []
+  end
+
+  def listToMap([h|t]) do
+    [%{chave: h, valor: 1}] ++ listToMap(t)
+  end
+
+  def exemplo() do
+    lista = [
+      %{chave: "a", valor: 1},
+      %{chave: "b", valor: 2},
+      %{chave: "a", valor: 3},
+      %{chave: "c", valor: 4},
+      %{chave: "b", valor: 5}
+    ]
+
+    #Para agrupar os mapas com chaves iguais
+    agrupados = Enum.group_by(lista, &(&1.chave))
+
+    #Depois disso, se você quiser juntar os mapas dentro de cada grupo
+    Enum.map(agrupados, fn {chave, mapas} -> {chave, soma(mapas)} end)
+
+  end
+
+  def agrupar(map) do
+    map
+      |>Enum.group_by(&(&1.chave))
+      |>Enum.map(fn {chave, mapas} -> {chave, soma(mapas)} end)
+  end
+
+  #Aqui, soma é uma função que você precisa definir para juntar
+  #os mapas com chaves iguais.
+  def soma(mapas) do
+    Enum.reduce(mapas, %{}, fn mapa, acc ->
+      Map.update(acc, :valor, mapa[:valor], &(&1 + mapa[:valor]))
     end)
   end
-end
 
-input = Enum.to_list(100_000..193_000)
-tasks = Utils.partitioning(input, 8)
-IO.inspect(tasks)
-map_results = Utils.runTasks(tasks)
-IO.inspect(map_results)
-reduce_result = Utils.reduce(map_results)
-IO.inspect(reduce_result)
-System.halt(0)
+  def result() do
+    input = Mapreduce.ler
+    tasks = partitioning(input, 8)
+    IO.inspect(tasks)
+    map_results = runtask(tasks)
+    IO.inspect(map_results)
+    System.halt(0)
+  end
+
+
+end
